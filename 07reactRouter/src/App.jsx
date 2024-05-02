6bfae999991ca3ad695de2f92e887b5e062b7a9e
Pranav Kumar <pranavkumar1522@gmail.com>
2024-05-02 19:45:48 +0530

    react router

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header"
+import Footer from './components/Footer/Footer'
+
+function App() {
+
+
+  return (
+    <>
+     <Header/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
