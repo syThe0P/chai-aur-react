57d01b934bf2f70dae21a061172fe5d23b1ea8a8
Pranav Kumar <pranavkumar1522@gmail.com>
2024-05-12 15:45:20 +0530

    setup redux and also the post and file setup of appwrite

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
+import { useDispatch} from 'react-redux';
 import "./App.css";
+import authService from './appwrite/auth';
+import { login, logout} from "./store/authSlice"
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return <>Blog website</>;
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false));
+  },[])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header/>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App;
